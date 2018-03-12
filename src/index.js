2960c15460f7c78b182c5ce42ed1fba8c430500f
Zach <zachschatz@gmail.com>
2018-03-12 15:53:45 -0400

    Change display of next player

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ class Board extends React.Component {
     });
   }
 
-  renderSquare(i) {
+  renderSquare(i) {    
     return (
       <Square 
         value={this.state.squares[i]} 
@@ -39,7 +39,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
